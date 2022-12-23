1c3cb42b0a347295fa8e2a97583cb43c09c202c5
lidechengguo <wanshishengyi33@163.com>
2022-12-23 20:00:55 +0800

    整体布局设计

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+// ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/app.css'
+Vue.use(Buefy)
+Vue.use(ElementUI)
+
 new Vue({
   router,
   store,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box">🔔 {{billboard.content}}</div>
+    <div class="columns">
+      <div class="column is-three-quarters">
+        <PostList></PostList>
+      </div>
+      <div class="column">
+        <Card></Card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import {getbillboard} from '@/api/billborad'
+import PostList from '@/views/post/Post'
+import Card from '@/views/card/Card'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{PostList,Card},
+  data(){
+    return{
+      billboard:{
+        content:""
+      }
+    }
+  },
+  methods:{
+    async fetchbillboard(){
+      getbillboard().then((resp)=>{
+        console.log(resp);
+        this.billboard.content = resp.data.content
+      })
+    }
+  },
+  created(){
+    this.fetchbillboard();
   }
 }
 </script>
