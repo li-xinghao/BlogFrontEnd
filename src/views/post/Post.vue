de037e94ed30b848305a1b9d78ae2e32005fd4a5
lidechengguo <wanshishengyi33@163.com>
2022-12-23 20:05:17 +0800

    整体布局设计

---
+++ src/views/post/Post.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="post">
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <span>卡片名称</span>
+            </div>
+            <div class="text item">
+                {{'列表内容'}}
+            </div>
+        </el-card>
+
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    name: 'Post',
+    data(){
+      return{
+       
+      }
+    },
+    methods:{
+      
+    },
+    created(){
+      
+    }
+  }
+  </script>
+  
